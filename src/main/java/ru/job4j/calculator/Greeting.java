bc7c15ba1462c68d701b73bd205d1340632f49a3
Andrey <a.e.golovin@mail.ru>
2022-01-02 15:42:38 +0700

    9.1. String [#237407]

---
+++ src/main/java/ru/job4j/calculator/Greeting.java
@@ -0,0 +1,12 @@
+package ru.job4j.calculator;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        idea = idea + " " + "But i am a newbie";
+        int year = 2022;
+        idea = idea + "But i am a newbie" + " " + year;
+        System.out.println(idea);
+    }
+}
