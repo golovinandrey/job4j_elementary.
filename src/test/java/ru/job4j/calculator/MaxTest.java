6ffe66132d889898c28ce7fd0826cec8a6489271
Andrey <a.e.golovin@mail.ru>
2022-01-10 16:09:11 +0700

    4.2. Тернарное сравнение [#189 #248378]

--- src/test/java/ru/job4j/calculator/MaxTest.java
+++ src/test/java/ru/job4j/calculator/MaxTest.java
@@ -13,4 +13,22 @@ public class MaxTest {
         int expected = 2;
         Assert.assertEquals(expected, result);
     }
+
+    @Test
+    public void whenMax2To1then2() {
+        int left = 2;
+        int right = 1;
+        int result = Max.max(2, 1);
+        int expected = 2;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenMax3To3then2() {
+        int left = 3;
+        int right = 3;
+        int result = Max.max(3, 3);
+        int expected = 3;
+        Assert.assertEquals(expected, result);
+    }
 }
