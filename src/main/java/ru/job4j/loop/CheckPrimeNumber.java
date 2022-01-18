08745d49c6dd2a391e2e730a7cc7c9e13dedca3e
Andrey <a.e.golovin@mail.ru>
2022-01-18 20:21:11 +0700

    5.5. Простое число [#171694 #251866]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -7,9 +7,6 @@ public class CheckPrimeNumber {
             if (number % i == 0) {
                 prime = false;
                 break;
-            } else {
-                prime = true;
-                break;
             }
         }
         return prime;
