b3a326f13109d04bca380617e009d582c8bc8032
Andrey <a.e.golovin@mail.ru>
2022-01-19 15:06:17 +0700

    5.7. Ипотека [#156315 #2314]

--- src/main/java/ru/job4j/calculator/TrgArea.java
+++ src/main/java/ru/job4j/calculator/TrgArea.java
@@ -10,5 +10,7 @@ public class TrgArea {
     public static void main(String[] args) {
         double result = TrgArea.area(2, 2, 2);
         System.out.println("area (2, 2, 2) = " + result);
+        result = TrgArea.area(3, 3, 3);
+        System.out.println("area(3, 3, 3) = " + result);
     }
 }
