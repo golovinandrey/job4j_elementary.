ffd7992297a64d2c074d162921726caa34ba5b19
Andrey <a.e.golovin@mail.ru>
2022-01-21 17:44:38 +0700

    5.9.1. Генерация конструкций в IDEA [#279134 #253684]

--- src/main/java/ru/job4j/array/ArrayLoop.java
+++ src/main/java/ru/job4j/array/ArrayLoop.java
@@ -2,12 +2,8 @@ package ru.job4j.array;
 
 public class ArrayLoop {
     public static void main(String[] args) {
-        int[] numbers = new int[5];
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = i * 2 + 3;
-        }
-        for (int number : numbers) {
-            System.out.println(number);
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
         }
     }
 }
