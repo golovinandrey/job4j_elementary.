b761d3dd80330c66965f04cb026a966b18bfca18
Andrey <a.e.golovin@mail.ru>
2022-01-21 17:54:47 +0700

    6.1. Классический поиск перебором. [#33489]

--- src/main/java/ru/job4j/array/ArrayLoop.java
+++ src/main/java/ru/job4j/array/ArrayLoop.java
@@ -2,8 +2,12 @@ package ru.job4j.array;
 
 public class ArrayLoop {
     public static void main(String[] args) {
-        for (int i = 0; i <= 10; i++) {
-            System.out.println(i);
+        int[] numbers = new int[5];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i * 2 + 3;
+        }
+        for (int number : numbers) {
+            System.out.println(number);
         }
     }
 }
