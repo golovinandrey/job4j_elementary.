917f01be19647e41e6031d5ad3c1e5566b47414b
Andrey <a.e.golovin@mail.ru>
2022-01-21 12:34:23 +0700

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/Definition.java
+++ src/main/java/ru/job4j/array/Definition.java
@@ -2,11 +2,13 @@ package ru.job4j.array;
 
 public class Definition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        System.out.println("Размер массива: " + ages.length);
-        String[] surnames = new String[100500];
-        System.out.println("Размер второго массива: " + surnames.length);
-        float[] prices = new float[40];
-        System.out.println("Размер третьего массива: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Andrew Golovin";
+        names[2] = "Pavel Markov";
+        names[3] = "Ivan Ivanov";
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
